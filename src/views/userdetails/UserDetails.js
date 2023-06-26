91c4f2903bbc2e98ac0f8f182bf7672cdca0a0c0
Chamesss <chamsedin.azouz@gmail.com>
2023-06-26 03:40:09 +0100

    build

--- src/views/userdetails/UserDetails.js
+++ src/views/userdetails/UserDetails.js
@@ -60,38 +60,38 @@ const UserDetailsPage = () => {
                 <FaChevronLeft className="carousel-arrow" />
             </div>
             {user && (
-            <div class="container d-flex justify-content-center align-items-center py-5">
-                <div class="row justify-content-center ">
-                    <div class=" profile-picture border border-2 rounded-circle shadow-lg p-3 mb-1 bg-white rounded" style={{ width: '150px', height: '150px' }}>
-                        <img src={user.picture} alt="Profile" class="rounded-circle" style={{ width: '100%', height: '100%' }} />
-                    </div>
-                    <div class="text-center pt-2 fs-5" >
-                        <span class="badge badge-pill bg-secondary">{user.firstname} {user.lastname}</span>
-                    </div>
-                    <div class="px-4 py-1">
-                        <hr class="my-4" style={{ borderWidth: '4px', fontWeight: 'bold' }} />
-                        <div class="col-lg-8">
-                            <div>
-                                {user.type === "individual" ? (
-                                    <div><p><FaHome /><strong> Type: </strong>Individuelle</p></div>
-                                ) : (<div><p><FaBuilding /><strong> Type: </strong>Entreprise</p></div>)}
-                                <p><FaPhoneAlt /><strong> Telephone:</strong> +216 {user.tel}</p>
-                                <p><FaMapMarkerAlt /><strong> Région:</strong> {user.country} </p>
-                                <p><FaCity /><strong> Ville:</strong> {user.city}</p>
-                                <p><FaMapMarkerAlt /><strong> Rejoint à:</strong> {new Date(user.createdAt).toLocaleDateString()}</p>
+                <div class="container d-flex justify-content-center align-items-center py-5">
+                    <div class="row justify-content-center ">
+                        <div class=" profile-picture border border-2 rounded-circle shadow-lg p-3 mb-1 bg-white rounded" style={{ width: '150px', height: '150px' }}>
+                            <img src={user.picture} alt="Profile" class="rounded-circle" style={{ width: '100%', height: '100%' }} />
+                        </div>
+                        <div class="text-center pt-2 fs-5" >
+                            <span class="badge badge-pill bg-secondary">{user.firstname} {user.lastname}</span>
+                        </div>
+                        <div class="px-4 py-1">
+                            <hr class="my-4" style={{ borderWidth: '4px', fontWeight: 'bold' }} />
+                            <div class="col-lg-8">
+                                <div>
+                                    {user.type === "individual" ? (
+                                        <div><p><FaHome /><strong> Type: </strong>Individuelle</p></div>
+                                    ) : (<div><p><FaBuilding /><strong> Type: </strong>Entreprise</p></div>)}
+                                    <p><FaPhoneAlt /><strong> Telephone:</strong> +216 {user.tel}</p>
+                                    <p><FaMapMarkerAlt /><strong> Région:</strong> {user.country} </p>
+                                    <p><FaCity /><strong> Ville:</strong> {user.city}</p>
+                                    <p><FaMapMarkerAlt /><strong> Rejoint à:</strong> {new Date(user.createdAt).toLocaleDateString()}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
             )}
             <div class="d-flex justify-content-center align-items-center">
                 <hr class="my-4" style={{ width: '90%', borderWidth: '4px', fontWeight: 'bold' }} />
             </div>
             {user && (
-            <div class="container pb-5">
-                <h3 style={{ color: "#D85A60" }}>Autre Annonces de <strong>{user.firstname}</strong>:</h3>
-            </div>
+                <div class="container pb-5">
+                    <h3 style={{ color: "#D85A60" }}>Autre Annonces de <strong>{user.firstname}</strong>:</h3>
+                </div>
             )}
             <div>
                 {ads.length > 0 ? (
