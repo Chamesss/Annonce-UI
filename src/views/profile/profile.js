6337da17881adfdbc8af2ec39eab998bed064a07
Chamesss <chamsedin.azouz@gmail.com>
2023-08-06 19:49:59 +0100

    profile

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaPhoneAlt, FaUpload } from "react-icons/fa";
+import PulseLoader from "react-spinners/PulseLoader";
+
 //import {FaCity, FaHome, FaBuilding, FaEnvelope, FaUser } from "react-icons/fa";
 
 // import { Spinner } from 'react-bootstrap';
@@ -8,12 +10,27 @@ import './css/Profile.css';
 
 function Profile({ userinfo }) {
   const [user] = useState(userinfo);
+  const [showformgeneralinfo, setShowformGeneralInfo] = useState(false);
+  const [image, setPicture] = useState();
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   // const [message, setMessage] = useState('');
   // const [isLoading, setIsLoading] = useState(false);
+  console.log(image);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, []);
+
+  useEffect(() => {
+    // If isLoading is true, show the loading animation for 1 second
+    if (isLoading) {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
+    }
+  }, [isLoading]);
 
   // const handleSendVerification = async (userid, email) => {
   //   setIsLoading(true);
@@ -32,6 +49,28 @@ function Profile({ userinfo }) {
 
   // }
 
+  const modifyGeneralInfo = () => {
+    if (!showformgeneralinfo) {
+      setIsLoading(true);
+    } else {
+      setSelectedImage('');
+    }
+    setShowformGeneralInfo(!showformgeneralinfo);
+  }
+
+  const handleImageChange = (e) => {
+    setPicture(e.target.files[0]);
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="profile-section-main">
       {user !== null ? (
@@ -44,21 +83,46 @@ function Profile({ userinfo }) {
                   <span>General info</span>
                 </div>
                 <div>
-                  <div className="button profile-edit-button" onClick={() => null}><AiFillEdit /></div>
+                  <div className="button profile-edit-button" onClick={modifyGeneralInfo}><AiFillEdit /></div>
                 </div>
               </div>
-              <div className="upper-profile-body-section pb-2">
-                <div className="upper-profile-body-section-info">
-                  <div style={{ maxWidth: '80px', maxHeight: '80px' }}>
-                    <img src={user.picture} alt="Profile" className="rounded-circle" style={{ width: '100%', height: '100%' }} />
-                  </div>
-                  <div className="upper-profile-section-details">
-                    <span className="upper-profile-section-details-name">{user.firstname} {user.lastname}</span>
-                    <span className="upper-profile-section-details-phone-adress"><FaPhoneAlt />&nbsp; +216 {user.tel}</span>
-                    <span className="upper-profile-section-details-phone-adress"><FaMapMarkerAlt />&nbsp; {user.country}, {user.city}</span>
-                  </div>
+              {isLoading ? (
+                <div className="d-flex justify-content-center align-items-center mt-4">
+                  <PulseLoader color="hsl(12, 88%, 59%)" />
                 </div>
-              </div>
+              ) : (
+                showformgeneralinfo ? (
+                  <div>
+                    <div className="d-flex justify-content-center align-items-center flex-column">
+                        <div className="profile-picture border border-2 rounded-circle shadow-lg bg-white rounded" style={{ width: '150px', height: '150px', position: 'relative', overflow: 'hidden' }}>
+                          {selectedImage ? (
+                            <img src={selectedImage} alt="Profile" className="rounded-circle" style={{ width: '100%', height: '100%' }} />
+                          ) : (
+                            <img src={user.picture} alt="Profile" className="rounded-circle" style={{ width: '100%', height: '100%' }} />
+                          )}
+                          <input type="file" id="picture" name="picture" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} />
+                          <label htmlFor="picture" className="upload-button container" style={{cursor:"pointer", backgroundColor: 'grey', width: '100%', height: '30px', position: 'absolute', bottom: 0, left: '50%', transform: 'translateX(-50%)' }}>
+                            <FaUpload style={{cursor:"pointer", position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />
+                          </label>
+                        </div>
+                        <p style={{marginTop:"20px", fontWeight:"500"}}>Upload new picture</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="upper-profile-body-section pb-2">
+                    <div className="upper-profile-body-section-info">
+                      <div style={{ maxWidth: '80px', maxHeight: '80px' }}>
+                        <img src={user.picture} alt="Profile" className="rounded-circle" style={{ width: '100%', height: '100%' }} />
+                      </div>
+                      <div className="upper-profile-section-details">
+                        <span className="upper-profile-section-details-name">{user.firstname} {user.lastname}</span>
+                        <span className="upper-profile-section-details-phone-adress"><FaPhoneAlt />&nbsp; +216 {user.tel}</span>
+                        <span className="upper-profile-section-details-phone-adress"><FaMapMarkerAlt />&nbsp; {user.country}, {user.city}</span>
+                      </div>
+                    </div>
+                  </div>
+                )
+              )}
             </div>
           </div>
 
